0252c3ef560ff8bf552bf168a02126111db4a581
aayush <hiaayush30@gmail.com>
2025-03-21 12:26:06 +0530

    commit

--- Portfolio-Sample Project/src/sections/Projects.tsx
+++ Portfolio-Sample Project/src/sections/Projects.tsx
@@ -37,23 +37,44 @@ const Projects: FC = () => {
       <div className="mt-10 md:mt-16 lg:mt-20">
         {projects.map(project => (
           <a href="#" key={project.name}
-            className="border-t last:border-b border-stone-400 border-dotted flex flex-col">
-            <div className="py-6 md:py-8">
+            className="border-t last:border-b border-stone-400 border-dotted flex flex-col relative group/project">
+            <div className="absolute bottom-0 left-0 w-full h-0 group-hover/project:h-full transition-all duration-500 bg-stone-300"></div>
+            <div className="py-6 md:py-8 relative">
               <div className="aspect-video md:hidden">
                 <Image className="size-full object-cover"
                   src={project.image} alt={project.name} />
               </div>
-              <div className="mt-8 md:mt-0 flex justify-between items-center">
-                <h3 className="text-2xl md:text-3xl">
-                  {project.name}
-                </h3>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none" viewBox="0 0 24 24"
-                  strokeWidth="1.5" stroke="currentColor"
-                  className="size-6">
-                  <path strokeLinecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                </svg>
+              <div className="mt-8 md:mt-0 flex justify-between items-center md:grid md:[grid-template-columns:1fr_300px_max-content] md:gap-8">
+                <div className="lg:group-hover/project:pl-4 transition-all duration-500">
+                  <h3 className="text-2xl md:text-3xl">
+                    {project.name}
+                  </h3>
+                </div>
+                <div className="relative">
+                  <div className="absolute aspect-video w-full top-1/2 -translate-y-1/2 show opacity-0 scale-90 group-hover/project:opacity-100 group-hover/project:scale-100 z-10 transition-all duration-500">
+                    <Image className="size-full object-cover"
+                      src={project.image} alt={project.name}
+                    />
+                  </div>
+                </div>
+                <div className="size-6 overflow-hidden">
+                  <div className="h-6 w-12 flex group-hover/project:-translate-x-1/2 transition-all duration-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none" viewBox="0 0 24 24"
+                      strokeWidth="1.5" stroke="currentColor"
+                      className="size-6">
+                      <path strokeLinecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none" viewBox="0 0 24 24"
+                      strokeWidth="1.5" stroke="currentColor"
+                      className="size-6">
+                      <path strokeLinecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                  </div>
+                </div>
               </div>
             </div>
           </a>
